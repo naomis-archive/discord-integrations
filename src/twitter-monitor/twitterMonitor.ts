b65996eb89c369b72f515df806f92eef4553b76c
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-08-04 18:48:46 -0700

    fix: remove duplicated tests

--- src/twitter-monitor/twitterMonitor.ts
+++ src/twitter-monitor/twitterMonitor.ts
@@ -1,20 +1,10 @@
 import { GlobalConfigInt } from "../interfaces/GlobalConfigInt";
 import { logHandler } from "../utils/logHandler";
 import { monitorTweets } from "./modules/monitorTweets";
-import { testTwitterWebhook } from "./modules/testTwitterWebhook";
 
 export const twitterMonitor = async (
   CONFIG: GlobalConfigInt
 ): Promise<void> => {
-  logHandler.log("debug", "Verifying Twitter webhook.");
-
-  const webhookValid = await testTwitterWebhook(CONFIG);
-
-  if (!webhookValid) {
-    logHandler.log("error", "Twitter webhook is not valid.");
-    return;
-  }
-
   logHandler.log("debug", "Starting first Twitter monitor run...");
   await monitorTweets(CONFIG);
 
--- src/wakatime-monitor/wakatimeMonitor.ts
+++ src/wakatime-monitor/wakatimeMonitor.ts
@@ -1,21 +1,11 @@
 import { GlobalConfigInt } from "../interfaces/GlobalConfigInt";
 import { logHandler } from "../utils/logHandler";
 import { handleWakatimeData } from "./modules/handleWakatimeData";
-import { testWakatimeWebhook } from "./modules/testWakatimeWebhook";
 
 export const wakatimeMonitor = async (
   CONFIG: GlobalConfigInt
 ): Promise<void> => {
-  logHandler.log("debug", "Validating Wakatime Webhook");
-
-  const webhookValid = await testWakatimeWebhook(CONFIG);
-
-  if (!webhookValid) {
-    logHandler.log("error", "Wakatime webhook is invalid.");
-    return;
-  }
-
-  logHandler.log("debug", "CRON job scheduled!");
+  logHandler.log("debug", "Wakatime Monitor Scheduled!");
 
   setInterval(async () => {
     logHandler.log("debug", "Running Wakatime Monitor");
